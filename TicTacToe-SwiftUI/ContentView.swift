41aa1e39faee069262a6919db35d25f4fb5ec9bf
safarsafarov <safarov.safar@hotmail.com>
2020-10-03 01:07:39 +0800

    finish

---
+++ TicTacToe-SwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TicTacToe-SwiftUI
+//
+//  Created by Safar Safarov on 2020/10/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TicTacToe-SwiftUI/TicTacToe_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_SwiftUIApp.swift
+//  TicTacToe-SwiftUI
+//
+//  Created by Safar Safarov on 2020/10/2.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
