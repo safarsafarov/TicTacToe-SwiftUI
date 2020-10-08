561d5a1420cd4ac712ee1701354def4796925f59
safarsafarov <safarov.safar@hotmail.com>
2020-10-08 11:35:37 +0800

    finish

--- TicTacToe-SwiftUI/ContentView.swift
+++ TicTacToe-SwiftUI/ContentView.swift
@@ -28,6 +28,10 @@ struct Home : View {
     // Moves
     @State var moves : [String] = Array(repeating: "", count: 9)
     @State var isPlaying = true
+    @State var gameOver = false
+    @State var msg = ""
+    
+    
     var body: some View {
         VStack {
             
@@ -44,7 +48,7 @@ struct Home : View {
                             .font(.system(size: 55))
                             .fontWeight(.heavy)
                             .foregroundColor(.white )
-                            .opacity(moves[index] == "" ? 1 : 0)
+                            .opacity(moves[index] != "" ? 1 : 0)
                     }
                     .frame(width: getWidth(), height: getWidth())
                     .cornerRadius(25)
@@ -52,10 +56,11 @@ struct Home : View {
                         .init(degrees: moves[index] != "" ? 180 : 0),
                         axis: /*@START_MENU_TOKEN@*/(x: 0.0, y: 1.0, z: 0.0)/*@END_MENU_TOKEN@*/,
                         anchor: .center,
-                        anchorZ: 0.0
+                        anchorZ: 0.0,
                         perspective: 1.0
-                    
+                    )
                     .onTapGesture(perform: {
+                        
                         withAnimation(Animation.easeIn(duration: 0.5)){
                             if moves[index] == ""{
                                 moves[index] = isPlaying ? "X" : "0"
@@ -68,6 +73,20 @@ struct Home : View {
             }
             .padding(15)
         }
+        .onChange(of: moves, perform: { value in
+            checkWinner()
+        })
+        .alert(isPresented: $gameOver, content: {
+            
+            Alert(title: Text("Winner"), message: Text(msg), dismissButton:
+                    .destructive(Text("Play Again"), action: {
+                withAnimation(Animation.easeIn(duration: 0.5)){
+                    moves.removeAll()
+                    moves = Array(repeating: "", count: 9)
+                    isPlaying = true
+                }
+            }))
+        })
     }
     
     func getWidth()->CGFloat{
@@ -75,5 +94,35 @@ struct Home : View {
         let width = UIScreen.main.bounds.width - (30 + 30)
         
         return width / 3
+    }
+    
+    func checkWinner(){
+        if checkMoves(player: "X"){
+            msg = "Player X Won !!!"
+            gameOver.toggle()
+        }
+        
+        if checkMoves(player: "0"){
+            msg = "Player 0 Won !!!"
+            gameOver.toggle()
+        }
+    }
+    
+    func checkMoves(player: String)->Bool{
+        // Horizontal moves...
+        for i in stride(from: 0, to: 9, by: 3) {
+            if moves[i] == player && moves[i + 1] == player && moves[i + 2] == player {
+                return true
+            }
+        }
+        
+        
+        // Vertical moves...
+        for i in 0...2 {
+            if moves[i] == player && moves[i + 3] == player && moves[i + 6] == player {
+                return true
+            }
+        }
+        return false
     }
 }
